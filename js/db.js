60c133a004e78dd6e8b5e8ea7c2a5c95d76876d3
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-10-17 18:39:32 +0800

    indexedDB

---
+++ js/db.js
@@ -0,0 +1,21 @@
+var dbPromised = idb.open("news-reader", 1, function (upgradeDb) {
+  var articlesObjectStore = upgradeDb.createObjectStore("articles", {
+    keyPath: "ID",
+  });
+  articlesObjectStore.createIndex("post_title", "post_title", {
+    unique: false,
+  });
+});
+function saveForLater(article) {
+  dbPromised
+    .then(function (db) {
+      var tx = db.transaction("articles", "readwrite");
+      var store = tx.objectStore("articles");
+      console.log("ARTICLENYA", article);
+      store.add(article.result);
+      return tx.complete;
+    })
+    .then(function () {
+      console.log("Artikel berhasil di simpan.");
+    });
+}
