af3e97dddd920aac760135b7dc888316d3922104
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-10-17 21:18:15 +0800

    update indexedDB

--- js/api.js
+++ js/api.js
@@ -82,16 +82,16 @@ function getArticles() {
 }
 
 function getArticleById() {
-  // Ambil nilai query parameter (?id=)
-  var urlParams = new URLSearchParams(window.location.search);
-  var idParam = urlParams.get("id");
-  console.log(idParam);
+  return new Promise(function (resolve, reject) {
+    // Ambil nilai query parameter (?id=)
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
 
-  if ("caches" in window) {
-    caches.match(base_url + "article/" + idParam).then(function (response) {
-      if (response) {
-        response.json().then(function (data) {
-          var articleHTML = `
+    if ("caches" in window) {
+      caches.match(base_url + "article/" + idParam).then(function (response) {
+        if (response) {
+          response.json().then(function (data) {
+            var articleHTML = `
             <div class="card">
               <div class="card-image waves-effect waves-block waves-light">
                 <img src="${data.result.cover}" />
@@ -102,21 +102,22 @@ function getArticleById() {
               </div>
             </div>
           `;
-          // Sisipkan komponen card ke dalam elemen dengan id #content
-          document.getElementById("body-content").innerHTML = articleHTML;
-        });
-      }
-    });
-  }
+            document.getElementById("body-content").innerHTML = articleHTML;
+            // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
+            resolve(data);
+          });
+        }
+      });
+    }
 
-  fetch(base_url + "article/" + idParam)
-    .then(status)
-    .then(json)
-    .then(function (data) {
-      // Objek JavaScript dari response.json() masuk lewat variabel data.
-      console.log(data);
-      // Menyusun komponen card artikel secara dinamis
-      var articleHTML = `
+    fetch(base_url + "article/" + idParam)
+      .then(status)
+      .then(json)
+      .then(function (data) {
+        // Objek JavaScript dari response.json() masuk lewat variabel data.
+        console.log(data);
+        // Menyusun komponen card artikel secara dinamis
+        var articleHTML = `
           <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
               <img src="${data.result.cover}" />
@@ -127,7 +128,134 @@ function getArticleById() {
             </div>
           </div>
         `;
-      // Sisipkan komponen card ke dalam elemen dengan id #content
-      document.getElementById("body-content").innerHTML = articleHTML;
+        document.getElementById("body-content").innerHTML = articleHTML;
+        // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
+        resolve(data);
+      });
+  });
+}
+
+function getAll() {
+  return new Promise(function (resolve, reject) {
+    dbPromised
+      .then(function (db) {
+        var tx = db.transaction("articles", "readonly");
+        var store = tx.objectStore("articles");
+        return store.getAll();
+      })
+      .then(function (articles) {
+        resolve(articles);
+      });
+  });
+}
+
+function getSavedArticles() {
+  getAll().then(function (articles) {
+    console.log(articles);
+    // Menyusun komponen card artikel secara dinamis
+    var articlesHTML = "";
+    articles.forEach(function (article) {
+      var description = article.post_content.substring(0, 100);
+      articlesHTML += `
+                  <div class="card">
+                  <a href="./article.html?id=${article.ID}&saved=true">
+
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img src="${article.cover}" />
+                      </div>
+                    </a>
+                    <div class="card-content">
+                      <span class="card-title truncate">${article.post_title}</span>
+                      <p>${description}</p>
+                    </div>
+                  </div>
+                `;
     });
+    // Sisipkan komponen card ke dalam elemen dengan id #body-content
+    document.getElementById("body-content").innerHTML = articlesHTML;
+  });
+}
+
+function loadPage(page) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      var content = document.querySelector("#body-content");
+
+      // tambahkan blok if berikut
+      if (page === "home") {
+        getArticles();
+      } else if (page === "saved") {
+        getSavedArticles();
+      }
+      // ---
+      if (this.status == 200) {
+        content.innerHTML = xhttp.responseText;
+      } else if (this.status == 404) {
+        content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+      } else {
+        content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
+      }
+    }
+  };
+  xhttp.open("GET", "pages/" + page + ".html", true);
+  xhttp.send();
+}
+
+function getSavedArticleById() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("id");
+
+  getById(idParam).then(function (article) {
+    articleHTML = "";
+    var articleHTML = `
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img src="${article.cover}" />
+      </div>
+      <div class="card-content">
+        <span class="card-title">${article.post_title}</span>
+        ${snarkdown(article.post_content)}
+      </div>
+    </div>
+  `;
+    // Sisipkan komponen card ke dalam elemen dengan id #content
+    document.getElementById("body-content").innerHTML = articleHTML;
+  });
+}
+
+function getById(id) {
+  return new Promise(function (resolve, reject) {
+    dbPromised
+      .then(function (db) {
+        var tx = db.transaction("articles", "readonly");
+        var store = tx.objectStore("articles");
+        return store.get(id);
+      })
+      .then(function (article) {
+        resolve(article);
+      });
+  });
+}
+
+function getSavedArticleById() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("id");
+
+  getById(idParam).then(function (article) {
+    articleHTML = "";
+    var articleHTML = `
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img src="${article.cover}" />
+      </div>
+      <div class="card-content">
+        <span class="card-title">${article.post_title}</span>
+        ${snarkdown(article.post_content)}
+      </div>
+    </div>
+  `;
+    // Sisipkan komponen card ke dalam elemen dengan id #content
+    document.getElementById("body-content").innerHTML = articleHTML;
+  });
 }
--- js/db.js
+++ js/db.js
@@ -6,16 +6,57 @@ var dbPromised = idb.open("news-reader", 1, function (upgradeDb) {
     unique: false,
   });
 });
+
 function saveForLater(article) {
   dbPromised
     .then(function (db) {
       var tx = db.transaction("articles", "readwrite");
       var store = tx.objectStore("articles");
-      console.log("ARTICLENYA", article);
+      console.log(article);
       store.add(article.result);
       return tx.complete;
     })
     .then(function () {
       console.log("Artikel berhasil di simpan.");
     });
+}
+
+function getAll() {
+  return new Promise(function (resolve, reject) {
+    dbPromised
+      .then(function (db) {
+        var tx = db.transaction("articles", "readonly");
+        var store = tx.objectStore("articles");
+        return store.getAll();
+      })
+      .then(function (articles) {
+        resolve(articles);
+      });
+  });
+}
+
+function getSavedArticles() {
+  getAll().then(function (articles) {
+    console.log(articles);
+    // Menyusun komponen card artikel secara dinamis
+    var articlesHTML = "";
+    articles.forEach(function (article) {
+      var description = article.post_content.substring(0, 100);
+      articlesHTML += `
+                  <div class="card">
+                    <a href="./article.html?id=${article.ID}">
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img src="${article.cover}" />
+                      </div>
+                    </a>
+                    <div class="card-content">
+                      <span class="card-title truncate">${article.post_title}</span>
+                      <p>${description}</p>
+                    </div>
+                  </div>
+                `;
+    });
+    // Sisipkan komponen card ke dalam elemen dengan id #body-content
+    document.getElementById("body-content").innerHTML = articlesHTML;
+  });
 }
